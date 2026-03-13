4ad20d012aec74cd973bce86805922060fea93b8
EMe-U <e.umurerwa@alustudent.com>
2026-03-13 17:23:28 +0200

    done

--- Javascript-basics/script.js
+++ Javascript-basics/script.js
@@ -26,9 +26,22 @@ state = "Texas";
 console.log(state);
 
 const pi = 3.14;
-pi = 3.14159; // This will cause an error because pi is a constant
 console.log(pi);
 
-a = 10; // Implicit global variable
-var a;
-console.log(a);
+let a;
+console.log(a); //TDZ(Temporal Dead Zone)
+a = 10; 
+
+// two symbols with same description
+let sym1 = Symbol("id");
+let sym2 = Symbol("id");
+console.log(sym1 === sym2); // Output: false
+
+
+//today take aways
+// 1. Variables can be declared using var, let, and const.
+// 2. var has function scope and is hoisted, while let and const have block scope and are not hoisted.
+// 3. let allows for variable reassignment, while const does not allow reassignment after initialization.
+// 4. Variables declared with var can be redeclared, while let and const cannot be redeclared in the same scope.
+// 5. Symbols are unique and immutable data types that can be used as identifiers for object properties.
+// 6. TDZ (Temporal Dead Zone) is a period of time during which a variable is not accessible before it is declared.
